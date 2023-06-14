8ce0268aa5ac087a89ae9954c3328bcea2fcbf02
Okashe <okashabdi88@gmail.com>
2023-06-14 10:00:22 -0500

    passing props

--- src/app.js
+++ src/app.js
@@ -1,31 +1,12 @@
+import Header from "./components/Header"
 function App(){
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const showComments = false;
-    const comments =[
-        {id:1, text: 'Comment one'},
-        {id:2, text: 'Comment two'},
-        {id:3, text: 'Comment three'}
-    ]
     return (
-        <div class='container'>
-         <h1>{title.toUpperCase()}</h1>
-         <p>{body}</p>
-        {showComments ?(
-            <div className="comments">
-            <h3>Comments({comments.length})</h3>
-            <ul>
-            {comments.map((comment, index)=>{
-                return(
-                    <li key={comment.id}>{comment.text}</li>
-                )
-            })}
-         </ul>
-        </div>
-        ):'No'}
-        
-        
-        </div>
+        <>
+        <Header  text='Hello World'/>
+          <div class='container'>
+            <h1>My App</h1>
+          </div>
+        </>
         
     )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = (props) => {
+  return (
+    <header>
+        <div className="container">
+           <h2>{props.text}</h2>
+        </div>
+    </header>
+  )
+}
+
+export default Header
