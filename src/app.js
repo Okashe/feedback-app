608967362316fdcd9f3c8c3f673fbaf81e398a33
Okashe <okashabdi88@gmail.com>
2023-06-14 09:41:56 -0500

    array - mapping

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 function App(){
     const title = 'Blog Post'
     const body = 'This is my blog post'
+    const comments =[
+        {id:1, text: 'Comment one'},
+        {id:2, text: 'Comment two'},
+        {id:3, text: 'Comment three'}
+    ]
     return (
         <div class='container'>
          <h1>{title.toUpperCase()}</h1>
          <p>{body}</p>
-         {Math.random()*(5+5)}
+        
+        <div className="comments">
+            <h3>Comments({comments.length})</h3>
+            <ul>
+            {comments.map((comment, index)=>{
+                return(
+                    <li key={comment.id}>{comment.text}</li>
+                )
+            })}
+         </ul>
+        </div>
+        
         </div>
         
     )
