661e4d4b5af26a1bccf164a1274b6228d61fc83b
Okashe <okashabdi88@gmail.com>
2023-06-14 11:05:55 -0500

    app level state

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -4,17 +4,11 @@ const FeedbackItem = () => {
     const [rating, setRating] = useState(7)
     const [text, setText] = useState('This is an example of a feedback')
 
-    const handleClick = () => {
-       setRating((prev) => {
-          console.log(prev)
-          return prev +1
-       })
-    }
   return (
     <div className="card">
         <div className="num-display">{rating}</div>
         <div className="text-display">{text}</div>
-        <button onClick={handleClick}>Click</button>
+        <button>Click</button>
     </div>
   )
 }
