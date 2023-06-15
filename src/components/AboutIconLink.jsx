2174fdaae5262d1f1c0424f09ff250fe41b739d0
Okashe <okashabdi88@gmail.com>
2023-06-14 19:22:03 -0500

    Link used instead of a tag to prevent refresh

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -1,11 +1,13 @@
 import { FaQuestion } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const AboutIconLink = () => {
   return (
+    //a tags make a page refresh each time so not good for internal routing. If it's external website , it's fine
     <div className="about-link">
-        <a href="/about">
+        <Link to="/about">
            <FaQuestion size={30} />
-        </a>
+        </Link>
         
     </div>
   )
